31a0f3c594eb1c77b2d18b9931e83a1e86655eb7
Calum J. Eadie <calum@calumjeadie.com>
2012-02-15 22:18:04 +0000

    Added license

--- computer-graphics-and-image-processing/Whiteboard.py
+++ computer-graphics-and-image-processing/Whiteboard.py
@@ -1,10 +1,18 @@
 #! /usr/bin/python
 
-# Follow Tkinter design pattern as using classes that are widgets rather than
-# creating high level object that would not be embeddable.
+"""Copyright (c) 2012 Calum J. Eadie <www.calumjeadie.com>
 
-# http://www.pythonware.com/library/tkinter/introduction/canvas.htm
-# http://effbot.org/tkinterbook/canvas.htm
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."""
+
+"""Follows Tkinter design pattern as using classes that are widgets rather than
+creating high level object that would not be embeddable.
+
+http://www.pythonware.com/library/tkinter/introduction/canvas.htm
+http://effbot.org/tkinterbook/canvas.htm"""
 
 from Tkinter import *
 
